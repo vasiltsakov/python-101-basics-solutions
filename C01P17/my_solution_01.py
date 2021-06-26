935ed276d0b50a2a114129a3c9fcda7e8dff0e24
Vasil Tsakov <vasil.tsakov@gmail.com>
2021-06-26 19:00:27 +0300

    P17 solution

--- C01P17/my_solution_01.py
+++ C01P17/my_solution_01.py
@@ -1,3 +1,15 @@
+def in_range(x, y, matrix):
+    min_x = 0
+    max_x = len(matrix)
+    min_y = 0
+    max_y = len(matrix[0])
+
+    if_range = x >= min_x and x < max_x and y >= min_y and y < max_y
+
+    return if_range
+
+
+
 def find_word(len_word, coordinates, direction, matrix):
     
     xd = 0
@@ -11,15 +23,43 @@ def find_word(len_word, coordinates, direction, matrix):
     if direction == 'e':
         xd = 0
         yd = 1
+    elif direction == 'w':
+        xd = 0
+        yd = -1
+    elif direction == 's':
+        xd = 1
+        yd = 0
+    elif direction == 'n':
+        xd = -1
+        yd = 0
+    elif direction == 'se':
+        xd = 1
+        yd = 1
+    elif direction == 'nw':
+        xd = -1
+        yd = -1
+    elif direction == 'ne':
+        xd = -1
+        yd = 1
+    elif direction == 'sw':
+        xd = 1
+        yd = -1
+    else:
+        return None
 
-        x, y = coordinates
+    x, y = coordinates
         
     for num in range(len_word):
-        
-        point = matrix[x][y]
-        next_word.append(point)
-        x = x + xd
-        y = y + yd
+
+        if_ra = in_range(x, y, matrix)
+
+        if if_ra:
+            point = matrix[x][y]
+            next_word.append(point)
+            x = x + xd
+            y = y + yd
+        else:
+            break
 
     return "".join(next_word)
 
@@ -29,15 +69,15 @@ def word_counter(matrix, word):
 
     direction = ['n', 's', 'w' , 'e', 'nw', 'ne', 'sw', 'se']
 
-    # for y, row in enumerate(matrix):
-    #     for x, col in enumerate(row):
-    #         for d in direction:
-    #             # print(f'x: {x}, y: {y}, d: {d}')
-    #             next_word = find_word(len(word), (x, y), d, matrix)
-    #             if next_word == word:
-    #                 result += 1
+    for y, row in enumerate(matrix):
+        for x, col in enumerate(row):
+            for d in direction:
+                next_word = find_word(len(word), (y, x), d, matrix)
+                if next_word == word:
+                    result += 1
 
-    result = find_word(len(word), (0, 0), 'e', matrix)
+    if word == word[::-1]:
+        result = result // 2
 
     return result
 
@@ -53,41 +93,41 @@ matrix = [
 result = word_counter(matrix, word)
 print(result, result == 3)
 
-# word = "actually"
-# matrix = [
-#     ["i", "v", "a", "n", "q", "h", "r", "e", "z", "g", "t", "z", "o", "y", "m"],
-#     ["e", "v", "n", "h", "t", "r", "x", "e", "k", "y", "d", "a", "i", "l", "c"],
-#     ["i", "a", "c", "t", "u", "a", "l", "l", "y", "m", "c", "x", "r", "l", "e"],
-#     ["m", "v", "c", "n", "p", "u", "a", "m", "n", "t", "l", "u", "e", "a", "a"],
-#     ["q", "r", "i", "t", "w", "e", "a", "q", "u", "p", "r", "x", "t", "u", "z"],
-#     ["p", "e", "a", "c", "t", "u", "a", "l", "l", "y", "w", "p", "y", "t", "m"],
-#     ["o", "y", "h", "t", "r", "e", "l", "u", "f", "p", "q", "n", "z", "c", "s"],
-#     ["p", "a", "c", "t", "u", "a", "l", "l", "y", "u", "r", "e", "q", "a", "r"]
-# ]
-# result = word_counter(matrix, word)
-# print(result, result == 4)
+word = "actually"
+matrix = [
+    ["i", "v", "a", "n", "q", "h", "r", "e", "z", "g", "t", "z", "o", "y", "m"],
+    ["e", "v", "n", "h", "t", "r", "x", "e", "k", "y", "d", "a", "i", "l", "c"],
+    ["i", "a", "c", "t", "u", "a", "l", "l", "y", "m", "c", "x", "r", "l", "e"],
+    ["m", "v", "c", "n", "p", "u", "a", "m", "n", "t", "l", "u", "e", "a", "a"],
+    ["q", "r", "i", "t", "w", "e", "a", "q", "u", "p", "r", "x", "t", "u", "z"],
+    ["p", "e", "a", "c", "t", "u", "a", "l", "l", "y", "w", "p", "y", "t", "m"],
+    ["o", "y", "h", "t", "r", "e", "l", "u", "f", "p", "q", "n", "z", "c", "s"],
+    ["p", "a", "c", "t", "u", "a", "l", "l", "y", "u", "r", "e", "q", "a", "r"]
+]
+result = word_counter(matrix, word)
+print(result, result == 4)
 
-# word = "madam"
-# matrix = [
-#     ["z", "v", "a", "n", "q", "h", "r", "e", "z", "g", "t", "z"],
-#     ["e", "v", "m", "h", "t", "r", "x", "e", "k", "y", "m", "a"],
-#     ["i", "a", "c", "a", "u", "a", "l", "l", "y", "a", "c", "x"],
-#     ["m", "v", "c", "n", "d", "u", "a", "m", "d", "t", "l", "u"],
-#     ["q", "t", "i", "t", "w", "a", "a", "a", "u", "p", "r", "x"],
-#     ["p", "e", "m", "a", "d", "a", "m", "l", "l", "y", "w", "p"],
-#     ["o", "y", "h", "t", "e", "e", "l", "u", "f", "p", "q", "n"],
-#     ["p", "a", "c", "t", "u", "a", "l", "l", "y", "u", "r", "e"]
-# ]
-# result = word_counter(matrix, word)
-# print(result, result == 3)
+word = "madam"
+matrix = [
+    ["z", "v", "a", "n", "q", "h", "r", "e", "z", "g", "t", "z"],
+    ["e", "v", "m", "h", "t", "r", "x", "e", "k", "y", "m", "a"],
+    ["i", "a", "c", "a", "u", "a", "l", "l", "y", "a", "c", "x"],
+    ["m", "v", "c", "n", "d", "u", "a", "m", "d", "t", "l", "u"],
+    ["q", "t", "i", "t", "w", "a", "a", "a", "u", "p", "r", "x"],
+    ["p", "e", "m", "a", "d", "a", "m", "l", "l", "y", "w", "p"],
+    ["o", "y", "h", "t", "e", "e", "l", "u", "f", "p", "q", "n"],
+    ["p", "a", "c", "t", "u", "a", "l", "l", "y", "u", "r", "e"]
+]
+result = word_counter(matrix, word)
+print(result, result == 3)
 
-# word = "python"
-# matrix = [
-#   ["r", "u", "b", "y"],
-#   ["r", "u", "b", "y"],
-#   ["r", "u", "b", "y"],
-#   ["r", "u", "b", "y"],
-# ]
+word = "python"
+matrix = [
+  ["r", "u", "b", "y"],
+  ["r", "u", "b", "y"],
+  ["r", "u", "b", "y"],
+  ["r", "u", "b", "y"],
+]
 
-# result = word_counter(matrix, word)
-# print(result, result == 0)
+result = word_counter(matrix, word)
+print(result, result == 0)
